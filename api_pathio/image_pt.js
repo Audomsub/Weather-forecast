c98cc11b5b4f0e3d023cef372370bc533731fef4
Audomsub <66022343@up.ac.th>
2025-05-02 11:35:36 +0700

    error

---
+++ api_pathio/image_pt.js
@@ -0,0 +1,48 @@
+const axios = require("axios");
+const path = require("path");
+const fs = require("fs-extra");
+const sharp = require("sharp");
+const pool = require("./db_pt"); 
+
+const DOWNLOAD_DIR = path.join(__dirname, "downloaded_images");
+const CONVERT_DIR = path.join(__dirname, "converted_images");
+
+fs.ensureDirSync(DOWNLOAD_DIR);
+fs.ensureDirSync(CONVERT_DIR);
+
+async function dowload_convertImage(imageUrl) {
+    try {
+        const filename = path.basename(imageUrl, path.extname(imageUrl)) + ".jpg";
+        const dowloadPath = path.join(DOWNLOAD_DIR, filename);
+        const convertPath = path.join(CONVERT_DIR, filename);
+
+        if (fs.existsSync(convertPath)) {
+            return convertPath;
+        }
+
+        const response = await axios.get(imageUrl, { responseType: "arraybuffer" });
+        await fs.writeFile(dowloadPath, response.data);
+
+        await sharp(dowloadPath).jpeg({ quality: 90 }).toFile(convertPath);
+
+        await pool.query(
+            "INSERT INTO images (filename, path) VALUES (?, ?)",
+            [filename, convertPath]
+        );
+
+        console.log("บันทึก path ลง MySQL แล้ว:", filename);
+        return convertPath;
+    } catch (err) {
+        console.error("Error processing image:", err);
+        return null;
+    }
+}
+
+function getConvertedImages() {
+    return fs.readdirSync(CONVERT_DIR).filter((file) => file.endsWith(".jpg"));
+}
+
+module.exports = {
+    dowload_convertImage,
+    getConvertedImages,
+};
